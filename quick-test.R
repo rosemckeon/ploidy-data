c2fe92b079ce8872e4d174a726a10cbe36555e4a
Rose McKeon <rose@rosemckeon.co.uk>
2019-08-22 16:58:40 +0000

    updated quick-test

--- quick-test.R
+++ quick-test.R
@@ -2,16 +2,13 @@ rm(list=ls())
 library(tidyverse)
 library(devtools)
 library(tictoc)
-# library(disturploidy)
-# or source the scripts from this dir as wd
-source("R/disturploidy.R")
-source("R/functions.R")
-source("R/traits.R")
+library(disturploidy)
+message("TMPDIR: ", tempdir())
 
 name <- "quick-test"
 generations <- 5
-simulations <- 1
-runs <- 3
+simulations <- 4
+runs <- 1
 
 # saves a new data file for every run which contains
 # the number of simulations specified. A separate
@@ -19,30 +16,19 @@ runs <- 3
 for(run in runs){
   this_run <- paste0(name, "-", run)
   disturploidy(
-    pop_size = 500,
+    pop_size = 250,
     grid_size = 10,
-    juvenile_selection_constant = .5,
-    adult_survival_prob = .7,
-    inbreeding_cost = 0,
-    germination_prob = .4,
-    seed_survival_prob = 0,
-    ploidy_prob = .5,
-    ploidy_growth_benefit = 0,
-    adult_size = 1.25,
-    N_ovules = 25,
-    pollen_range = 10,
-    fertilisation_prob = .75,
-    uneven_matching_prob = .75,
-    selfing_diploid_prob = 0,
-    selfing_polyploid_prob = 0,
-    triploid_mum_prob = .75,
+    pollen_range = 9,
     seed_dispersal_range = 9,
+    ploidy_prob = .1,
+    ploidy_growth_benefit = .5,
+    inbreeding_cost = 0.5,
+    selfing_polyploid_prob = 0.75,
+    triploid_mum_prob = .2,
     disturbance_freq = 2,
     generations = generations,
     simulations = simulations,
     filename = this_run,
-    filepath = "simulations/data/",
-    logfilename = this_run,
-    logfilepath = "simulations/data/logs/"
+    logfilename = this_run
   )
 }
